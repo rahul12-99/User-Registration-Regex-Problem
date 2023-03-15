957f47b661aa7e6b3f5114381d23007865641e13
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-16 02:03:55 +0530

    Added : Code for check first name

---
+++ src/com/userregistrationusingregex/RegexProblem.java
@@ -0,0 +1,22 @@
+package com.userregistrationusingregex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexProblem {
+
+    void firstName(){
+        // Pattern for Name start with cap and has minimum 3 char.
+        Pattern p = Pattern.compile("[A-Z][a-z]{2,}");
+        // Matcher for giving input.
+        Matcher m = p.matcher("Rahul");
+        // boolean value for check matching or not.
+        boolean b = m.matches();
+        System.out.println(b);
+    }
+
+    public static void main(String[] args) {
+        RegexProblem regex = new RegexProblem();
+        regex.firstName();
+    }
+}
