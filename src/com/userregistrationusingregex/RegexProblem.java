a1ba21bc7bd305c46edfb8c19f898994e3bf16d6
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-16 20:17:04 +0530

    Added : Code for check valid email

--- src/com/userregistrationusingregex/RegexProblem.java
+++ src/com/userregistrationusingregex/RegexProblem.java
@@ -5,11 +5,13 @@ import java.util.regex.Pattern;
 
 public class RegexProblem {
 
-    void lastName(){
-        // Pattern for Name start with cap and has minimum 3 char.
-        Pattern p = Pattern.compile("[A-Z][a-z]{2,}");
+    void email(){
+        /* Pattern E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co)
+         and 2 optional (xyz & in) with precise @ and . positions */
+
+        Pattern p = Pattern.compile("[a-z0-9+.+a-z]+@+[bl+.+co+.+in]{2,}");
         // Matcher for giving input.
-        Matcher m = p.matcher("Kumar");
+        Matcher m = p.matcher("abc.xyz@bl.co.in");
         // boolean value for check matching or not.
         boolean b = m.matches();
         System.out.println(b);
@@ -17,6 +19,6 @@ public class RegexProblem {
 
     public static void main(String[] args) {
         RegexProblem regex = new RegexProblem();
-        regex.lastName();
+        regex.email();
     }
 }
