9d0b02eeea8df5b025c1563498b67ecfef58de95
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-21 20:23:05 +0530

    Added : Code for varify that all emails provided

--- src/com/userregistrationusingregex/RegexProblem.java
+++ src/com/userregistrationusingregex/RegexProblem.java
@@ -6,19 +6,32 @@ import java.util.regex.Pattern;
 
 public class RegexProblem {
 
-    void password(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter password");
-        String password = sc.nextLine();
-        // pattern for password exactly 1 special char;
-        Pattern p = Pattern.compile("((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=[^@#$%^&+!=]*[@#$%^&+!=][^@#$%^&+!=]*$)).{8,}");
-        Matcher m = p.matcher(password);
-        boolean b = m.matches();
-        System.out.println(b);
+
+    public void verifyEmailSamples() {
+        String[] validEmails = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com",
+                "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au",
+                "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"};
+
+        String[] inValidEmails = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
+                ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com",
+                "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"};
+
+        System.out.println("For valid emails");
+
+        for (int i = 0; i < validEmails.length; i++) {
+            System.out.println("Result for emails : " + (i + 1) + " "
+                    + Pattern.matches("[a-z-+0-9]+[.]?[a-z0-9]+[@][a-z0-9]+[.][a-z]{2,4}[.]?[a-z]{0,3}", validEmails[i]));
+        }
+        System.out.println();
+        System.out.println("For Invalid Emails ");
+        for (int i = 0; i < inValidEmails.length; i++) {
+            System.out.println("Result for emails : " + (i + 1) + " "
+                    + Pattern.matches("[a-z-+0-9]+[.]?[a-z0-9]+[@][a-z0-9]+[.][a-z]{2,4}[.]?[a-z]{0,3}", inValidEmails[i]));
+        }
     }
 
     public static void main(String[] args) {
         RegexProblem regex = new RegexProblem();
-        regex.password();
+        regex.verifyEmailSamples();
     }
 }
